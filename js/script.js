c9d2744cf51b2c2c4bdde03efad900ef4ba383a0
Beniamin Guras <gurasbeniamin@gmail.com>
2022-03-22 16:51:55 +0100

    Adjust css js html

--- js/script.js
+++ js/script.js
@@ -43,21 +43,20 @@ function displayResult(argPlayerMove, argComputerMove) {
 console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
 
 if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
-
 printMessage('Wygrywasz!');
-
+return true; 
 } else if (argPlayerMove == 'kamień' && argComputerMove == 'nozyce') {
-
 printMessage('Wygrywasz!');
-
+return true;
 } else if (argPlayerMove == 'nozyce' && argComputerMove == 'papier') {
-
 printMessage('Wygrywasz!');
-
+return true;
 } else if (argPlayerMove == argComputerMove) {
-    printMessage('Remis!');
+printMessage('Remis!');
+return null;
 } else {
 printMessage('Przegrywasz :(!');
+return false;
 }
 printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 }
@@ -72,13 +71,22 @@ randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
 computerMove = getMoveName(randomNumber);
 console.log('ruch komputera to: ' + computerMove);
-displayResult(playerMove, computerMove);
+result = displayResult(playerMove, computerMove);
+if(result == true){
+    playerPoints = playerPoints + 1;
+} else if (result == false){
+    computerPoints = computerPoints + 1; 
+}
+printMessage(playerPoints + ":" + computerPoints);
 }
 
-var argComputerMove, argMoveId, argPlayerMove, computerMove, playerInput, playerMove, randomNumber, buttonPaper, buttonRock, buttonScissors;
+var argComputerMove, argMoveId, argPlayerMove, computerMove, playerInput, playerMove, randomNumber, buttonPaper, buttonRock, buttonScissors, computerPoints, playerPoints, result;
 buttonRock = document.getElementById('button-rock');
 buttonRock.addEventListener('click', function(){ buttonClicked('kamień'); });
 buttonPaper = document.getElementById('button-paper');
 buttonPaper.addEventListener('click', function(){ buttonClicked('papier'); });
 buttonScissors = document.getElementById('button-scissors');
 buttonScissors.addEventListener('click', function(){ buttonClicked('nozyce'); });
+
+computerPoints = 0;
+playerPoints = 0;
